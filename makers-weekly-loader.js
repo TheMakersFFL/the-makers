a4581d5cb0d7164784976b6f0e9ca5c14746a3da
MiscExpFFL <tomtom2023@yahoo.com>
2026-09-24 18:00:32 -0700

    Cache-bust Makers audit runtime

--- makers-weekly-loader.js
+++ makers-weekly-loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  const bust='makers-20260924-phase4';
+  const bust='makers-20260924-audit1';
   const load=file=>new Promise((resolve,reject)=>{
     const s=document.createElement('script');
     s.src=file+(file.includes('?')?'&':'?')+'v='+bust;
